e20ecb13e30e085760e65f8d737a74eb7b881747
mrudula1604 <git config --global user.email chilukuri.sat@northeastern.edu>
2023-02-16 00:06:22 -0500

    assignment2 first ui commit

---
+++ InsuranceCompany/src/Business/Pet.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business;
+
+/**
+ *
+ * @author Prasad
+ */
+public class Pet {
+    
+	private String PetName;
+	private double Age;
+	private String Breed;
+	private String Type;
+	private String gender;
+}
---
+++ InsuranceCompany/src/Business/Vaccine.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business;
+
+/**
+ *
+ * @author Prasad
+ */
+public class Vaccine {
+    
+private String vaccineName;
+private double dosage;
+    
+}
